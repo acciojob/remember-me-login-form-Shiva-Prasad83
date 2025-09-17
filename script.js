363c7add622b3f253a41722d6b53543971b30abb
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-17 14:15:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const form=document.getElementById("login-form");
+const rememberMe=document.getElementById("checkbox");
+const userName=document.getElementById("username");
+const password=document.getElementById("password");
+const existingButton=document.getElementById("existing");
+// console.log(form[2].checked);
+form.addEventListener("submit",(e)=>{
+	e.preventDefault();
+// console.log(form[2].checked);
+	if(!rememberMe.checked){
+		alert(`Logged in as ${userName.value}`);
+		localStorage.clear();
+	}else{
+		localStorage.setItem("username",userName.value);
+		localStorage.setItem("password",password.value);
+		localStorage.setItem("rememberMe","checked");
+		window.location.reload();
+	}
+	form.reset();
+});
+window.addEventListener("load",(e)=>{
+	if(localStorage.getItem("rememberMe")=="checked"){
+	existingButton.classList.add("visible");
+	}
+});
+existingButton.addEventListener("click",(e)=>{
+	alert(`Logged in as ${localStorage.getItem("username")}`);
+})
